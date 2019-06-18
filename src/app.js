a6af50f0c3d7063b4d669eed7ddb3e260173f1a0
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2019-06-19 04:39:31 +1200

    basic html form created, express.js installed to route get and post methods, node_modules ignored

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-const http = require('http');
-const port = process.env.PORT || 3000
+const http = require('http'); // Includes HTTP module which allows node.js to transfer data over the HTTP.
+const port = process.env.PORT || 3000 // Let port be whatever is in the environment variable PORT or localhost 3000
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((req, res) => { // Creates an HTTP server that listens to server ports and gives a response back to the client.
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+  res.setHeader('Content-Type', 'text/html'); // Set file to HTML
+  res.end('<h1>Hello World</h1>'); // Header
 });
 
 server.listen(port,() => {
