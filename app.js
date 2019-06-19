a2f6dcbb2339787453317aca33a9758e95e5db8c
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2019-06-19 19:20:52 +1200

    edited app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const express = require('express');
+const app = express();
 const http = require('http'); // Includes HTTP module which allows node.js to transfer data over the HTTP.
 const port = process.env.PORT || 3000 // Let port be whatever is in the environment variable PORT or localhost 3000
 
@@ -7,6 +9,11 @@ const server = http.createServer((req, res) => { // Creates an HTTP server that 
   //res.setHeader('Content-Type', 'text/html'); // Set file to HTML
   //res.end('<h1>Hello World</h1>'); // Header
 });
+
+app.use(express.static('public'));
+app.get('/main.html', function(req,res){
+  res.sendFile(__dirname + "/" + "index.htm");
+})
 
 server.listen(port,() => {
   console.log(`Server running at port `+port);
