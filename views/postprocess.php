3352454e376df9b77310a10e8113de2141171f68
Jeeerus <40648688+Jeeerus@users.noreply.github.com>
2019-06-19 21:59:21 +1200

    NavBar
    Added a beastly NavBar that will make every visitor fall in love with the website

---
+++ views/postprocess.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Handles the form posting into the
+ * database
+ */
+
+// TODO
+    // START CONNECTION
+
+    // VALIDATE DATABASE
+
+    // VALIDATE FORM DATA
+
+    // POST TO DATABASE
+?>
