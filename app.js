af8e4714f2ea34c1482177bd80d56c127f36a92b
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2019-06-19 22:02:00 +1200

    changed form to textarea

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ var app = express(); // Stores the variable/function 'express' into the variable
 // process.env.PORT for whatever port Heroku gives and port:3000 as our local port.
 var port = process.env.PORT || 4000;
 
-// express.static is a built-in middleware function that serves static files/assets (css,js,html).
+// set engine used for viewable page - EJS in this case.
 app.set('view engine', 'ejs');
+// express.static is a built-in middleware function that serves static files/assets (css,js,html).
 app.use(express.static(__dirname + '/public'));
+// app.get is a function that routes HTTP GET requests to the specified path with the specified callback functions. first param = root directory
 app.get('/',function(req,res ) {
   res.render('main');
 });
