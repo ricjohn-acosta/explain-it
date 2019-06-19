48ca5d9b72981019840e7cd6ac3c69fb3370bead
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2019-06-19 23:59:18 +1200

    separated home page from post page

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 // Creates an Express application - a framework for Node.js that allows for server based applications that will listen for any i/o reqs from users.
 var express = require('express'); // require() is a Node.js function. Passing in 'express' parameter loads the express module.
 var app = express(); // Stores the variable/function 'express' into the variable 'app'.
+var path = require('path');
+var routes = require('./routes/main')
+var post = require('./routes/post')
 
 // Set the port of application
 // process.env.PORT for whatever port Heroku gives and port:3000 as our local port.
@@ -9,12 +12,11 @@ var port = process.env.PORT || 4000;
 
 // set engine used for viewable page - EJS in this case.
 app.set('view engine', 'ejs');
+
 // express.static is a built-in middleware function that serves static files/assets (css,js,html).
-app.use(express.static(__dirname + '/public'));
-// app.get is a function that routes HTTP GET requests to the specified path with the specified callback functions. first param = root directory
-app.get('/',function(req,res ) {
-  res.render('main');
-});
+app.use(express.static('public'));
+app.use('/', routes);
+app.use('/post', post);
 
 app.listen(port, function() {
   console.log('Server running on localhost:' + port);
