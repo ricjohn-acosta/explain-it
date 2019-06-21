d28b5525ab2f4eeff01b9f2c0e44b6adb6e23a45
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2019-06-21 22:00:20 +1200

    db added

--- app.js
+++ app.js
@@ -2,38 +2,27 @@
 // Creates an Express application - a framework for Node.js that allows for server based applications that will listen for any i/o reqs from users.
 var express = require('express'); // require() is a Node.js function. Passing in 'express' parameter loads the express module.
 var app = express(); // Stores the variable/function 'express' into the variable 'app'.
-var routes = require('./controllers/main')
-var post = require('./controllers/post')
-var mysql = require('mysql');
 var bodyParser = require('body-parser');
+var routes = require('./controllers/main');
+var post = require('./controllers/post');
 
 // Set the port of application
 // process.env.PORT for whatever port Heroku gives and port:3000 as our local port.
 var port = process.env.PORT || 4000;
 
-// Connect to remote mysql
-function getConnection() {
-  return mysql.createConnection ({
-    host:"remotemysql.com",
-    port: 3306,
-    user:"nXE9gAAK9C",
-    password:"6FY4uxRfnD",
-    database: "nXE9gAAK9C"
-  })
-}
-var connection = getConnection();
-connection.connect();
 
 // Set engine used for viewable page - EJS in this case.
 app.set('view engine', 'ejs');
 
 // express.static is a built-in middleware function that serves static files/assets (css)
 app.use(express.static('public'));
-app.use('/', routes);
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/post', post);
-app.use(bodyParser.urlencoded({extended:false}));
+app.use('/', routes);
 
 
-app.listen(port, function() {
+
+app.listen(port, function () {
   console.log('Server running on localhost:' + port);
 });
+
--- controllers/post.js
+++ controllers/post.js
@@ -1,16 +1,39 @@
 // app.get is a function that routes HTTP GET requests to the specified path with the specified callback functions. first param = root directory second param = callback function
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
 
-router.get('/', function(req,res, next) {
+// Connect to remote mysql
+function getConnection() {
+    return mysql.createConnection({
+      host: 'remotemysql.com',
+      user: 'nXE9gAAK9C',
+      password: '2nLLI1fG21',
+      database: 'nXE9gAAK9C',
+      port: '3306'
+    })
+  }
+  
+  var connection = getConnection();
+  connection.connect((err) => {
+    if (err) {
+      throw err;
+    }
+    console.log('mysql connected..');
+  });
+  
+
+router.get('/', function (req, res, next) {
     res.render('post');
 })
 
-router.post('/post', (req, res) => {
+router.post('/', (req, res) => {
     const title = req.body.title;
     const explanation = req.body.explanation;
-    const queryString = "INSERT INTO nXE9gAAK9C (title,content) VALUES(?,?)"
-    getConnection().query(queryString, [title, explanation],);
+    const queryString = "INSERT INTO post (post_title,content) VALUES(?,?)"
+    getConnection().query(queryString, [title, explanation]);
+    res.render('post');
     res.end();
 })
+
 module.exports = router;
