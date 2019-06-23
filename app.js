5836071d3e181fc33c80a6efd16c51167790a2cb
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2019-06-24 01:40:45 +1200

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 
-/** Declare global variables **/
+/* Declare global variables */
 var express = require('express'); // require() is a Node.js function. Passing in 'express' parameter loads the express module.
 var app = express(); // Stores the variable/function 'express' into the variable 'app'.
 var bodyParser = require('body-parser');
 var main = require('./controllers/mainRouter');
 var post = require('./controllers/postRouter');
 
-/** Server **/
+/* Server */
 var port = process.env.PORT || 4000; // process.env.PORT for whatever port Heroku gives and port:3000 as our local port.
 app.listen(port, function () {
   console.log('Server running on localhost:' + port);
 });
 
-/** Processing middlewares **/
+/* Processing middlewares */
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Set engine used for viewable page - EJS in this case.
+/* Set engine used for viewable page - EJS in this case. */
 app.set('view engine', 'ejs');
 
-/** User request middlewares **/
+/* User request middlewares */
 app.use('/post', post);
 app.use('/', main);
 
