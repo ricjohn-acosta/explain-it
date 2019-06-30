67779ac18b93814060d24472b040c0bfbd667f7e
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2019-06-30 19:17:57 +1200

    added bootstrap

--- models/postModel.js
+++ models/postModel.js
@@ -2,9 +2,11 @@ var express = require('express');
 var router = express.Router();
 var connection = require('../config/dbConnection');
 
+/* Connects to MySQL database */
 connection.checkConnection();
 connection.connect();
 
+/* Creates a page where a list of JSON can be found which is then fetched by the frontend fetch() */
 router.get('/', async function (req, res, next) {
     await connection.connect().query("SELECT content FROM post", function (err, rows, fields) {
         if (err) {
